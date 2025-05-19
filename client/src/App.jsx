27c4b1f25820ef06b5a47eb154356c9b8e5f9558
Nav Neet Pal <137805006+navneetpal2003@users.noreply.github.com>
2025-05-19 19:43:23 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem", background: "#111", color: "#fff" }}>
+      <h1>Ortus Tattoo Studio</h1>
+      <p>This is a tattoo studio website frontend.</p>
+    </div>
+  );
+}
+
+export default App;
